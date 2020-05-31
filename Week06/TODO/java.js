25051e0b1e6f03f55c3549a4e0578c96aa215dbc
jordan-hamblin <64113676+jordan-hamblin@users.noreply.github.com>
2020-05-30 23:33:16 -0600

    ToDo

--- Week06/TODO/java.js
+++ Week06/TODO/java.js
@@ -1,85 +1,123 @@
 
+//Modified some ideas from W3 Schools
 
-
-window.onload = function(){
-    document.getElementById("action").addEventListener('change', show);
-    document.getElementById("do_something").addEventListener('click', doSomething);
-    localStorage.setItem("Make a To Do List","0");
+function createClose() {
+    let listItems = document.getElementsByTagName("LI");
+    let i;
+    for (i = 0; i < listItems.length; i++) {
+        let span = document.createElement("SPAN");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        listItems[i].appendChild(span);
+    }
 }
 
+// Click on a close button to hide the current list item
+function clickClose() {
+    let close = document.getElementsByClassName("close");
+    let j;
+    for (j = 0; j < close.length; j++) {
+        close[j].onclick = function () {
+            let div = this.parentElement;
+            div.remove();
+        }
+    }
 
-function doSomething(){
-    let action = document.getElementById("action").value;
+
+}
+
+window.onload = function () {
+
+    masterList = [];
+    masterList = document.getElementsByTagName("LI");
+
+    let initialList = JSON.parse(localStorage.getItem("li's"));  //Will need to put this in place of above code when it works
     
-    if (action == "insert"){
-        
-        let item = document.getElementById("item").value;    
-
-        window.localStorage.setItem("0","item");
-        show();
+    for (i = 0; i < initialList.length; i++) {
+        let li = document.createElement("li");
+        let t = document.createTextNode(initialList[i]);
+        li.appendChild(t);
+        document.getElementById("myUL").appendChild(t);
     }
+
+    createClose();
+    clickClose();
+    let list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+    this.showAll();
 }
 
-function show(event){
-    let action = document.getElementById("action").value;
-    if (action == "display"){
-            document.getElementById("display").style.display = "block";
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
 
-                
-                let list = window.localStorage.getItem("0");
-                document.getElementById("display").value = list;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Blank To do List Items Don't Do Much Good!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
 
-            // list.forEach(element => {
-            //     document.getElementById("display").innerHTML += element;
-            //     let check = document.createElement("INPUT");
-            //     check.setAttribute("type", "checkbox");
-            //     document.getElementById("display").appendChild(check);
-            //     document.getElementById("display").innerHTML += "<br>";                 
-            // });
-            
-            
-    }
-    else{
-        document.getElementById("display").style.display = "none";
-    }
-    if (action == "complete"){
-        document.getElementById("complete").style.display = "block";
-    }
-    else{
-        document.getElementById("complete").style.display = "none";
-    }
-    if (action == "delete"){
-        document.getElementById("delete").style.display = "block";
-    }
-    else{
-        document.getElementById("delete").style.display = "none";
-    }
-    if (action == "insert"){
-        document.getElementById("insert").style.display = "block";
-    }
-    else{
-        document.getElementById("insert").style.display = "none";
-    }   
+    createClose();
+    clickClose();
+    masterList = document.getElementsByTagName("LI");
+    
 }
 
-
-
-
-
-
-function loadStory(){
-    let storyName = document.getElementById("name_input").value;
-    let storyHTML = localStorage.getItem(storyName);
-    document.getElementById("story_editor").value = storyHTML;
+function showAll() {
+    let i;
+    let j = 0
+    for (i = 0; i < masterList.length; i++) {
+        masterList[i].style.visibility = "visible";
+        j++;
+    }
+    j += " Tasks Left";
+    document.getElementById("tasks_left").innerHTML = j;
 }
 
-function saveStory(){
-    let storyName = document.getElementById("name_input").value;
-    let storyHTML = document.getElementById("story_editor").value;
-    localStorage.setItem(storyName, storyHTML);
+function showActive() {
+    let i;
+    let j = 0;
+    for (i = 0; i < masterList.length; i++) {
+        if (masterList[i].className == "checked") {
+            masterList[i].style.visibility = "hidden";
+        }
+        else {
+            masterList[i].style.visibility = "visible";
+            j++;
+        }
+    }
+    j += " Tasks Left";
+    document.getElementById("tasks_left").innerHTML = j;
 }
 
-function displayStory(){
-    let storyHTML = document.getElementById("story_editor").value;
-    document.getElementById("story_display").innerHTML = storyHTML;
+function showComplete() {
+    let i;
+    let j = 0;
+    for (i = 0; i < masterList.length; i++) {
+        if (masterList[i].className == "checked") {
+            masterList[i].style.visibility = "visible";
+            j++;
+        }
+        else {
+            masterList[i].style.visibility = "hidden";
+        }
+    }
+    j += " Tasks Left";
+    document.getElementById("tasks_left").innerHTML = j;
 }
+
+window.onunload = function () {
+    masterList = document.getElementsByTagName("LI");
+    let temp = Array.prototype.slice.call( masterList )
+    localStorage.setItem("li's", JSON.stringify(temp));
+}
+
